2e2f0572361fbdf3e9bc1d33fee5b701aec70278
Shahzaib khan <shahzaibkhan@shahzaibs-macbook.local>
2020-12-08 11:32:50 +0500

    Initial Commit

---
+++ Tic_Tac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic_Tac
+//
+//  Created by Shahzaib khan on 08/12/2020.
+//  Copyright © 2020 Shahzaib khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
